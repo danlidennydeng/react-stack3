04f13497f67b998c16f657a62568cc2b7a29158f
danlidennydeng <danlideng@yahoo.com>
2021-06-16 22:35:07 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello Stack3!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Stack3 is here to stay</p>
     </div>
   );
 }
